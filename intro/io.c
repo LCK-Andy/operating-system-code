4ca0ee882990b4d68c274bc12db7a376eeff2c05
SW/Andy <li.ck8819@gmail.com>
2025-01-14 10:43:36 +0200

    did some lab

---
+++ intro/io.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
+
+int main(int argc, char* argv[]){
+    int fd = open("/tmp/file", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+    assert(fd >= 0);
+
+    char buffer[20];
+    sprintf(buffer, "hello World\n");
+
+    int rc = write(fd, buffer, strlen(buffer));
+
+    assert(rc == strlen(buffer));
+    fsync(fd);
+    close(fd);
+    return 0;
+}
